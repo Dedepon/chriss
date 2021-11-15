09841cdbe59d3833ac8caf21d74fd043f6ae487b
Denis Bourguignon <bourguignon.denis.2012@gmail.com>
2021-11-15 22:22:28 +0100

    Fis style & date

--- src/app/services/order/order.service.ts
+++ src/app/services/order/order.service.ts
@@ -1,23 +1,34 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
-import { tap } from "rxjs/operators";
-import { Order } from "src/app/models/order";
-import { ORDER_LIST_URL } from "./constants";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { Order } from 'src/app/models/order';
+import { ORDER_LIST_URL } from './constants';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class OrderService {
   public orders$: Observable<Order[]>;
 
-  private ordersSubject$: BehaviorSubject<Order[]> = new BehaviorSubject<Order[]>([]);
+  private ordersSubject$: BehaviorSubject<Order[]> = new BehaviorSubject<
+    Order[]
+  >([]);
   constructor(private http: HttpClient) {
     this.orders$ = this.ordersSubject$.asObservable();
   }
 
   public loadOrders(): Observable<Order[]> {
-    return this.http.get<Order[]>(ORDER_LIST_URL).pipe(tap((orders: Order[]) => this.ordersSubject$.next(orders)));
+    return this.http.get<Order[]>(ORDER_LIST_URL).pipe(
+      map(
+        (orders: Order[]) =>
+          orders.map((o: Order) => {
+            o.date = o.date ? new Date(o.date).toLocaleString() : '';
+            return o;
+          }),
+        tap((orders: Order[]) => this.ordersSubject$.next(orders))
+      )
+    );
   }
 
   public createOrder(order: Order): Observable<Order> {
@@ -25,14 +36,17 @@ export class OrderService {
   }
 
   public updateOrder(order: Order): Observable<Order> {
-    return this.http.put<Order>(ORDER_LIST_URL + "/" + order._id, order);
+    return this.http.put<Order>(ORDER_LIST_URL + '/' + order._id, order);
   }
 
   public closeOrder(order: Order): Observable<Order> {
-    return this.http.put<Order>(ORDER_LIST_URL + "/" + order._id + "/close", {});
+    return this.http.put<Order>(
+      ORDER_LIST_URL + '/' + order._id + '/close',
+      {}
+    );
   }
 
   public deleteOrder(order: Order): Observable<Order> {
-    return this.http.delete<Order>(ORDER_LIST_URL + "/" + order._id);
+    return this.http.delete<Order>(ORDER_LIST_URL + '/' + order._id);
   }
 }
